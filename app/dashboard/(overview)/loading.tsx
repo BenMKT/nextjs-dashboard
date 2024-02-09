9ab664fdbd7710cb9e80bbc33698e3a6ce238336
Benson Kiai <benson.kiai86@gmail.com>
2024-02-09 22:41:34 +0300

    Learn how to stream a specific component using `<Suspense>`

--- app/dashboard/(overview)/loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardSkeleton from "@/app/ui/skeletons"; // Import the DashboardSkeleton component as a fallback for the loading state(loading skeletons).
-
-const Loading = () => {
-  return <DashboardSkeleton />;
-};
-
-export default Loading;
