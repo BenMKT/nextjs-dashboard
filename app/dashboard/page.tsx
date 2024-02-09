de878bcc2f4d64c36687984fca745f3544f48b57
Benson Kiai <benson.kiai86@gmail.com>
2024-02-09 23:18:49 +0300

    Practice by Streaming <LatestInvoices>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react'; // Suspense allows you to defer rendering parts of your application until some condition is met (e.g. data is loaded). Wrap your dynamic components in Suspense then pass it a fallback component to show while the dynamic component loads.
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
   // const revenue = await fetchRevenue(); // Fetch data inside its respective component thus revenue-chart.tsx
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -34,7 +34,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
