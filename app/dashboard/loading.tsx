0bf8d715aa1d5dbdb667700a8610558323d111df
Benson Kiai <benson.kiai86@gmail.com>
2024-02-09 07:41:27 +0300

    Learn adding loading skeletons

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import DashboardSkeleton from "@/app/ui/skeletons"; // Import the DashboardSkeleton component as a fallback for the loading state(loading skeletons).
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return <DashboardSkeleton />;
 };
 
 export default Loading;
