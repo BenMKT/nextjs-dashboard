95ec1ed644d76f2071fd856ac197be28b7b3b093
Benson Kiai <benson.kiai86@gmail.com>
2024-02-17 10:07:28 +0300

    Learn search optimization by adding `use-debounce` for search input that  limits the rate at which the search function can fire

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -8,7 +10,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  // Using debouncing prevents a new database query on every keystroke, thus saving resources.
+  const handleSearch = useDebouncedCallback((term) => {
+    // console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -16,7 +21,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 1000);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
