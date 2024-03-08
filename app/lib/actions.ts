ccce1e0b9f856f2b501dba3a60ab52bb329eb9c1
Benson Kiai <benson.kiai86@gmail.com>
2024-03-08 08:12:15 +0300

    Learn extracting the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,8 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {};
+export async function createInvoice(formData: FormData) {
+    // Extract and Convert FormData to a plain object
+    const rawFormData = Object.fromEntries(formData.entries());
+    // Test it out in the console terminal
+    console.log(rawFormData);
+};
