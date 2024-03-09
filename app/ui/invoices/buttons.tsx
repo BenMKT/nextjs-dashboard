6c2a0dc0c151b62329e908342b92a3cbcaa3d0dd
Benson Kiai <benson.kiai86@gmail.com>
2024-03-09 20:40:35 +0300

    Learn updating an invoice: first create a new dynamic route segment with the invoice `id`

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,7 +16,7 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`} // update the href to accept the id prop by using template literals to link to a dynamic route segment
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
