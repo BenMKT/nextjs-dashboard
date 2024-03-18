4280469fc782bc2bba4782120b407b14398ad63f
Benson Kiai <benson.kiai86@gmail.com>
2024-03-19 00:36:06 +0300

    Learn improving accessibiliy: through Server-Side form validation for create- invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,16 +9,19 @@ import { z } from 'zod';
 // define a schema that matches the shape of your database invoice table object using Zod
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a customer' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 // define a schema that matches the shape of your create form object
-const CreateInvoice = FormSchema.pick({
-  customerId: true,
-  amount: true,
-  status: true,
+const CreateInvoice = FormSchema.omit({
+  id: true,
+  date: true,
 });
 // define a schema that matches the shape of your edit form object
 const UpdateInvoice = FormSchema.omit({
@@ -26,14 +29,37 @@ const UpdateInvoice = FormSchema.omit({
   date: true,
 });
 
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 // define a function/action to create a new invoice
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   // Extract and Convert FormData to a plain object
-  const RawFormData = Object.fromEntries(formData.entries());
+  // use const RawFormData = Object.fromEntries(formData.entries()); if you have many form fields
+  const RawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
   // Pass RawFormData to CreateInvoice to validate the types
-  const rawFormData = CreateInvoice.parse(RawFormData);
+  const validatedFields = CreateInvoice.safeParse(RawFormData);
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   // Convert the amount into cents as its good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy
-  const amountInCents = rawFormData.amount * 100;
+  const amountInCents = amount * 100;
   // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
   const date = new Date().toISOString().split('T')[0];
   // Test it out in the console terminal
@@ -41,11 +67,11 @@ export async function createInvoice(formData: FormData) {
   // create an SQL query, with error handling, to insert the new invoice into your database and pass in the variables
   try {
     await sql`
-      INSERT INTO invoices (customer_id, amount, status, date) VALUES (${rawFormData.customerId}, ${amountInCents}, ${rawFormData.status}, ${date})  
+      INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})  
     `;
   } catch (error) {
     return {
-      message: 'Database Error: Failed to Create Invoice.'
+      message: 'Database Error: Failed to Create Invoice.',
     };
   }
   // Once the database has been updated, the `/dashboard/invoices` path should be revalidated allowing fresh data to be fetched from the server to reflect the changes
