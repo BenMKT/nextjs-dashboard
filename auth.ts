bead5b7b0328d7b5dac6a96a6c9b043bade9ee7e
Benson Kiai <benson.kiai86@gmail.com>
2024-03-24 05:39:38 +0300

    Learn authentication:  by adding the Credentials provider

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
-// created a separate file for the bcrypt package because bcrypt relies on Node.js APIs not available in Next.js Middleware
+// Create a separate file for the `bcrypt` package because `bcrypt` relies on Node.js APIs not available in Next.js Middleware
 
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+// Add the Credentials provider which allows users to log in with a username and a password. It's generally recommended to use alternative providers such as `OAuth` or `email` providers
+import Credentials from 'next-auth/providers/credentials';
 
 export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
+  ...authConfig, providers: [Credentials({})],
 });
